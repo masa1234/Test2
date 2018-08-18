221f53c8b1fbab58db1b40235ab22fb694572415
masa <masa@HOPESVR>
2018-08-18 12:29:50 +0900

    修正

--- src/Test3.java
+++ src/Test3.java
@@ -1,6 +1,9 @@
-public class Test3{
-  public static void main(String[] args){
-    System.out.println("Test3");
-  }
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		System.out.println("Test3");
+
+	}
 }
-    
+
